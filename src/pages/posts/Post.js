8d40ae842cb26d740abc7e67ce0dee6755582331
Kollecollier <kristoffer.collier@live.se>
2022-10-25 15:00:51 +0000

    add comment component, testing when home

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,9 +2,10 @@ import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Post = (props) => {
     const {
@@ -25,7 +26,22 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+    
+    const history = useHistory();
 
+    const handleEdit = () => {
+      history.push(`/posts/${id}/edit`);
+    };
+
+    const handleDelete = async () => {
+      try {
+        await axiosRes.delete(`/posts/${id}/`);
+        history.goBack();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
     const handleLike = async () => {
         try {
             const { data } = await axiosRes.post('/likes/', { post:id });
@@ -59,19 +75,25 @@ const Post = (props) => {
       };
 
 
-  return ( 
-  <Card className={styles.Post}>
-    <Card.Body>
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
         <Media className="align-items-center justify-content-between">
-        <Link to={`/profiles/${profile_id}`}>
-        <Avatar src={profile_image} height={55} />
-        {owner}
-        </Link>
-        <div className="d-flex align-items-center">
-        <span>{updated_at}</span>
-        {is_owner && postPage && "..."}
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && (
+              <MoreDropdown
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+              />
+            )}
         </div>
         </Media>
+
     </Card.Body>
     <Link to={`/posts/${id}`}>
     <Card.Img src={image} alt={title} />
